679c5050dd2e72b47832ab68bf96f46f511d1cf2
ZY <E0032221@u.nus.edu>
2017-08-20 18:42:31 +0800

    Fixed incomplete comments in Person class and changed access modifier of accessor methods to none - restricting access within package and subclasses

--- src/seedu/addressbook/Person.java
+++ src/seedu/addressbook/Person.java
@@ -3,26 +3,26 @@ package seedu.addressbook;
 import java.util.HashMap;
 
 /**
- * This class is meant to simplify demonstration for LO-Collections and LO-enum
+ * This class is meant to simplify demonstration for LO-Collections and LO-enum.
  */
-public class Person {
+class Person {
     /**
      * Key values for the Person property storage. The enum is public for use from other classes.
      */
-    public enum PersonProperty {NAME, PHONE, EMAIL};
+    enum PersonProperty {NAME, PHONE, EMAIL}
 
     /**
-     * The HashMap storage system for the properties. If
+     * The HashMap storage system for the properties.
      */
     private HashMap<PersonProperty, String> fields;
 
     /**
      * Constructor for Person class.
      * @param name - String value of the person's name.
-     * @param phone - String value of the person's phone number
-     * @param email - String value of the person's
+     * @param phone - String value of the person's phone number.
+     * @param email - String value of the person's email.
      */
-    public Person(String name, String phone, String email) {
+    Person(String name, String phone, String email) {
         fields = new HashMap<>(3, 1);
         fields.put(PersonProperty.NAME, name);
         fields.put(PersonProperty.PHONE, phone);
@@ -31,10 +31,10 @@ public class Person {
 
     /**
      * Accessor method to get value for any enum PersonProperty key.
-     * @param key - key of type PersonProperty
+     * @param key - key of type PersonProperty.
      * @return String value of the property or "N/A" if invalid key.
      */
-    public String getProperty(PersonProperty key) {
+    String getProperty(PersonProperty key) {
         String tryProperty = fields.get(key);
         if (tryProperty != null) {
             return tryProperty;
@@ -47,7 +47,7 @@ public class Person {
      * Accessor method to get name of the person.
      * @return String value of the name or "N/A" if invalid.
      */
-    public String getName() {
+    String getName() {
         String tryName = fields.get(PersonProperty.NAME);
         if (tryName != null) {
             return tryName;
@@ -60,7 +60,7 @@ public class Person {
      * Accessor method to get phone number of the person.
      * @return String value of the phone number or "N/A" if invalid.
      */
-    public String getPhoneNumber() {
+    String getPhoneNumber() {
         String tryPhone = fields.get(PersonProperty.PHONE);
         if (tryPhone != null) {
             return tryPhone;
@@ -73,7 +73,7 @@ public class Person {
      * Accessor method to get email of the person.
      * @return String value of the email or "N/A" if invalid.
      */
-    public String getEmail() {
+    String getEmail() {
         String tryEmail = fields.get(PersonProperty.EMAIL);
         if (tryEmail != null) {
             return tryEmail;
