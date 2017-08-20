74b40db3c4fe0fc3694abac5e2528486728f3d05
ZY <E0032221@u.nus.edu>
2017-08-20 19:41:37 +0800

    Implemented a more loose find operation where arguments are no longer required to be case-sensitive.

--- src/seedu/addressbook/AddressBook.java
+++ src/seedu/addressbook/AddressBook.java
@@ -506,7 +506,7 @@ public class AddressBook {
      * @return set of keywords as specified by args
      */
     private static Set<String> extractKeywordsFromFindPersonArgs(String findPersonCommandArgs) {
-        return new HashSet<>(splitByWhitespace(findPersonCommandArgs.trim()));
+        return new HashSet<>(splitByWhitespace(findPersonCommandArgs.toLowerCase().trim()));
     }
 
     /**
@@ -518,7 +518,7 @@ public class AddressBook {
     private static ArrayList<Person> getPersonsWithNameContainingAnyKeyword(Collection<String> keywords) {
         final ArrayList<Person> matchedPersons = new ArrayList<>();
         for (Person person : getAllPersonsInAddressBook()) {
-            final Set<String> wordsInName = new HashSet<>(splitByWhitespace(getNameFromPerson(person)));
+            final Set<String> wordsInName = new HashSet<>(splitByWhitespace(getNameFromPerson(person).toLowerCase()));
             if (!Collections.disjoint(wordsInName, keywords)) {
                 matchedPersons.add(person);
             }
