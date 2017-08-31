dff5fd9000b8ea957714f68abe5124c02ecff107
Alex Ang <E0032221@u.nus.edu>
2017-09-01 04:52:50 +0800

    Update imports
    Removed unused comparator import from java.util

--- src/seedu/addressbook/AddressBook.java
+++ src/seedu/addressbook/AddressBook.java
@@ -21,7 +21,6 @@ import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashSet;
 import java.util.Optional;
 import java.util.Scanner;
